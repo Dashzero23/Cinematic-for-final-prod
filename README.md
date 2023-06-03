3b6daf2abb666ea387ce824116167f626308d186
Dashzero23 <114184567+Dashzero23@users.noreply.github.com>
2023-06-02 22:39:35 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Where to play your game (a link to your deployed game) : https://gabriel-yunjia.github.io/Title-and-Victory-screen-design/ 
+
+
+Ending/Start menu scenes for our final game, although its just text, it will follow the same concepts. 
+
+Gabriel Yunjia, Jeevithan Mahenthran, Nguyen Vu
---
+++ scenes/Scene1.js
@@ -0,0 +1,19 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super('Scene1');
+    }
+   
+    create() {
+        this.add.text(game.config.width/2, 260, 'Cinematic (WIP)', { font: '24px Futura', fill: '#FFFFFF' }).setOrigin(0.5);
+
+
+    }
+
+
+
+    update() {
+        
+
+        
+    }
+}
